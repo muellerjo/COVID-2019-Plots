9ec7f81060a69856b782cdd7bd8897081b0440f5
muellerjo <41830496+muellerjo@users.noreply.github.com>
2020-03-15 11:45:12 +0000

    plots in R

---
+++ R-Plots/01_confirmed-per-country.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+library(scales)
+library(reshape2)
+
+DATALOG <- read.csv("C:/Users/Mueller/Dropbox/Privat-Hobby/IT/GitHubRepos/COVID-2019-Plots/03_complete-data-pop-days1.CSV")
+
+#DATALOG$x..Confirmed <- (DATALOG$Confirmed / (DATALOG$.pop2020*1000))*100
+
+#ggplot(DATALOG, aes(x=days, y=Confirmed, group=country, color=country))+ geom_line(aes())
+
+germany <- DATALOG[DATALOG$country == "Germany" |
+                     DATALOG$country == "Italy"| 
+                     DATALOG$country == "Spain"| 
+                     DATALOG$country == "Switzerland"
+                   ,]
+#germany
+
+ggplot(germany, aes(x=days1, y=X..Confirmed, group=country, color=country))+
+  geom_line(aes())+ labs(title= "COVID-2019",
+                         subtitle = "Confirmed cases in percent of population",
+                         y="% of population", 
+                         x = "Days since first confirmed case",
+                         caption="@muellertag \n Data Source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data")
